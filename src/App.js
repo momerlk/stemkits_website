299d2231246625199b04a0d75448a140aa310f24
Omer Ali Malik <omeralimalik96@gmail.com>
2022-12-22 12:48:24 +0500

    initial look and feel done!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react"
+import {Button , AppBar , Grid} from "@mui/material";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Kit from "./components/kit"
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary : {
+      main : "#7776e6",
+    },
+  },
+});
+
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    };
+  }
+
+  render(){
+    return (
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <main>
+          <div className='App'>
+            <AppBar position='fixed'>
+              <Button>test</Button>
+            </AppBar>
+            <div className='landing'>
+            </div>
+
+            <div className='browse-title'>
+              <h1>Kits</h1>
+            </div>
+
+            {/* <Grid container spacing={1}>
+              <Grid item xs="auto">
+                <Kit image="https://imgur.com/a/mMos19O" name="Kit 1" description="This is the STEM Kit 1"/>
+              </Grid>
+              <Grid item xs="auto">
+                <Kit image="https://imgur.com/a/mMos19O" name="Kit 1" description="This is the STEM Kit 1"/>
+              </Grid>
+            </Grid> */}
+            <div className='flex-container'>
+              <Kit image="https://imgur.com/a/mMos19O" name="STEM Kit 1" description="Description of STEM Kit 1"/>
+              <Kit image="https://imgur.com/a/mMos19O" name="STEM Kit 2" description="Description of STEM Kit 2"/>
+            </div>
+            
+
+            <div className='footer'>
+              <span>Developed by Omer Ali Malik</span>
+            </div>
+          </div>
+        </main>
+      </ThemeProvider> 
+    )
+  }
 }
+
+
 
 export default App;
---
+++ src/components/kit.js
@@ -0,0 +1,42 @@
+import {Card , CardActions , CardMedia , CardContent , Button , Typography} from "@mui/material";
+import React from "react";
+
+class KitComponent extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return (
+            <div>
+                <Card sx={{ maxWidth: 800 , marginLeft : 2}}>
+                    <CardMedia
+                        component="img"
+                        alt="Kit Image"
+                        height="400"
+                        src="https://i0.wp.com/sciencestore.pk/wp-content/uploads/2020/08/kit-3.0.jpg?fit=1650%2C1275&ssl=1"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {this.props.name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {this.props.description} 
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small" variant="contained" onClick={
+                            () => {
+                                
+                            }
+                        }
+                        >Add to Cart</Button>
+                    </CardActions>
+                    </Card>
+            </div>
+        )
+    }
+}
+
+export default KitComponent;
